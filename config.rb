8ba77c7ef36f7164f9f414ef1a809f032e33d503
Sam Gibson <sam@ifdown.net>
2017-01-01 16:00:49 +1100

    Grumble, grumble

--- config.rb
+++ config.rb
@@ -52,7 +52,48 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
 require "markdown_checkboxes"
-renderer = CheckboxMarkdown.new(Redcarpet::Render::HTML.new())
+require "action_view"
+# renderer = CheckboxMarkdown.new(Redcarpet::Render::HTML.new())
+class Redcarpet::Markdown
+  include ActionView::Helpers::FormTagHelper
+  alias_method :render_orig, :render
+
+  def render(text, &block)
+    text = render_checkboxes(text, &block)
+    render_orig text
+  end
+
+  def render_checkboxes(text, &block)
+    checkbox_regex  = /\*\s?\[(x|\s)\]/
+
+    text.gsub(checkbox_regex).with_index do |current_match, current_index|
+      checked = current_match =~ /x/ ? true : false
+
+      body =
+        text.gsub(checkbox_regex).with_index do |match, index|
+          if index == current_index
+            checked ? "* [ ]" : "* [x]"
+          else
+            match
+          end
+        end
+
+      check_box_tag "check_#{current_index}", "", checked, data: data_options(body, &block)
+    end
+  end
+
+  def data_options(body)
+    if block_given?
+      data_struct = DataStruct.new
+      yield(data_struct, body)
+      data_struct.serializable_hash
+    else
+      {}
+    end
+  end
+end
+
+renderer = Redcarpet::Render::HTML.new()
 
 set :markdown_engine, :redcarpet
 set :markdown, input: "GFM", tables: true, autolink: true, renderer: renderer
